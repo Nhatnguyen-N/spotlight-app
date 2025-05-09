c9079ac13e14b23ae0aed5ea92bb63a075ecbe97
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-05-09 21:42:00 +0700

    Clerk:login with google

---
+++ components/InitialLayout.tsx
@@ -0,0 +1,19 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { router, Stack, useSegments } from "expo-router";
+import { useEffect } from "react";
+
+export default function InitialLayout() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  const segments = useSegments();
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inAuthScreen = segments[0] === "(auth)";
+    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)/login");
+    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+  }, [isLoaded, isSignedIn, segments]);
+
+  if (!isLoaded) return null;
+  return <Stack screenOptions={{ headerShown: false }} />;
+}

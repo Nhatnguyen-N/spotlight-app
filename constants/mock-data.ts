b091e5d051480177525527ae1e6136314f1dfeed
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-05-11 22:29:57 +0700

    feed, post for homeScreen

---
+++ constants/mock-data.ts
@@ -0,0 +1,44 @@
+export const STORIES = [
+  {
+    id: "1",
+    username: "You",
+    avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=200",
+    hasStory: false,
+  },
+  {
+    id: "2",
+    username: "Alex",
+    avatar: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=200",
+    hasStory: true,
+  },
+  {
+    id: "3",
+    username: "Taylor",
+    avatar: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=200",
+    hasStory: true,
+  },
+  {
+    id: "4",
+    username: "Jordan",
+    avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=200",
+    hasStory: false,
+  },
+  {
+    id: "5",
+    username: "Casey",
+    avatar: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=200",
+    hasStory: true,
+  },
+  {
+    id: "6",
+    username: "Riley",
+    avatar: "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=200",
+    hasStory: true,
+  },
+  {
+    id: "7",
+    username: "Morgan",
+    avatar: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=200",
+    hasStory: false,
+  },
+];
